16a8e36fb3d7e82d18af03334d69c0f6a7fa5d5e
Nicholas-Darko-Brown <nicholas.brown@amalitech.com>
2022-06-07 13:45:27 +0000

    "feat: created routes for pages"

--- star-wars-challenge/src/App.tsx
+++ star-wars-challenge/src/App.tsx
@@ -1,11 +1,25 @@
-import './App.css';
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import ListerPage from "./pages/ListerPage/ListerPage";
+import Details from "./pages/ListerPage/Details";
+import Context from "./pages/ListerPage/Context";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Context>
+    <Router>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/listerPage" element={<ListerPage />} />
+          <Route path="/details" element={<Details />} />
+        </Routes>
+      <Footer />
+    </Router>
+    </Context>
   );
-}
+};
 
 export default App;
