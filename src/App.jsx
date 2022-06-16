1d5a072567328f4f795ef57b1a5373f72135929d
Godfred Nsabo <godfred.nsabo@amalitech.com>
2022-06-16 17:08:28 +0000

    refactor: chnaged the routers position

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import Footer from "./components/Footer/Footer";
-import Header from "./components/Header/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import ListerPage from "./pages/ListerPage/ListerPage";
-import Details from "./pages/ListerPage/Details";
-import Context from "./pages/ListerPage/Context";
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import ListerPage from './pages/ListerPage/ListerPage';
+import Details from './pages/ListerPage/Details';
+import Context from './pages/ListerPage/Context';
 
 const App = () => {
   return (
     <Context>
-            <Router>
-          <Header />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/listerPage" element={<ListerPage />} />
-            <Route path="/details" element={<Details />} />
-          </Routes>
-          <Footer />
-        </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/listerPage" element={<ListerPage />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
     </Context>
   );
 };
