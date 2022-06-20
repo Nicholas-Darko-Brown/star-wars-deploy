68c7badcee89af4a01ae9ded6d5f6e99cfd07930
Nicholas-Darko-Brown <nicholas.brown@amalitech.com>
2022-06-20 13:04:50 +0000

    feat: added detail search button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,30 @@
-import {
-  Container,
-  Nav,
-  Navbar
-} from "react-bootstrap";
+import { FC } from "react";
+import { Container, Form, Nav, Navbar } from "react-bootstrap";
 import starWarsLogo from "../../assets/star-wars-logo.png";
 import { useNavigate } from "react-router-dom";
 import { FaList } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
+import { PeopleState } from "../../pages/ListerPage/Context";
 
-const Header: React.FC = () => {
+const Header: FC = () => {
+  const { visited } = PeopleState();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
+  const handleChange = (e: any) => {
+    fetch(e.target.value);
+    navigate("/details");
+  };
 
   return (
     <header>
       <Navbar bg="light" expand="lg" fixed="top">
         <Container fluid>
-          <Navbar.Brand onClick={() => {
-            navigate("/")
-          }}>
+          <Navbar.Brand
+            onClick={() => {
+              navigate("/");
+            }}
+          >
             <img
               alt=""
               src={starWarsLogo}
@@ -26,7 +32,6 @@ const Header: React.FC = () => {
               height="30"
               className="d-inline-block align-top"
             />
-            
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
@@ -35,10 +40,43 @@ const Header: React.FC = () => {
               style={{ maxHeight: "100px" }}
               navbarScroll
             >
-              <Nav.Link href="" onClick={() => {
-                navigate("/listerPage")
-              }} className="lister">Lister Page  <FaList />  </Nav.Link>
+              <Nav.Link
+                href=""
+                onClick={() => {
+                  navigate("/listerPage");
+                }}
+                className="lister"
+              >
+                Lister Page <FaList />{" "}
+              </Nav.Link>
+              <Nav.Link
+                href=""
+                onClick={() => {
+                  navigate("/searchBox");
+                }}
+                className="lister"
+                style={{marginLeft: "10px"}}
+              >
+                Detail Search <FiSearch />{" "}
+              </Nav.Link>
             </Nav>
+
+            
+
+            <Form.Select
+              aria-label="Default select example"
+              onChange={handleChange}
+              className="dropdown"
+            >
+              <option className="visited-pages">Visited Detail Pages</option>
+              {visited.map((visit: any, i: number) => (
+                <option key={i}>
+                  {visit.character +
+                    " - " +
+                    visit.id.substring(29, visit.id.length - 1)}
+                </option>
+              ))}
+            </Form.Select>
           </Navbar.Collapse>
         </Container>
       </Navbar>
